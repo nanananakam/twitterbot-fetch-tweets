c8a1b87baf87116669f313e86076b706ddf8046b
naoto nakamura <naotomain@gmail.com>
2019-04-09 23:33:31 +0900

    lambdaに変更

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-aws s3 cp s3://${AWS_S3_BUCKET}/tweets.tar.xz .
-tar Jxvf tweets.tar.xz
-rm tweets.tar.xz
-/main
-tar Jcvf tweets.tar.xz tweets.db
-aws s3 cp tweets.tar.xz s3://${AWS_S3_BUCKET}/tweets.tar.xz
