87880fa0c933bca33e36568b34f3dae6d3af29b7
naoto nakamura <naotomain@gmail.com>
2019-04-09 23:50:42 +0900

    lambda化

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/ChimeraCoder/anaconda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"io"
 	"net/url"
 	"os"
+	"os/exec"
 )
 
 func main() {
@@ -15,8 +20,34 @@ func main() {
 		TwitterID string `gorm:"unique_index"`
 		Tweet     string `gorm:"type:varchar(512)"`
 	}
+	svc := s3.New(session.New(), &aws.Config{
+		Region: aws.String(os.Getenv("AWS_DEFAULT_REGION")),
+	})
 
-	db, err := gorm.Open("sqlite3", "tweets.db")
+	s3file, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
+		Key:    aws.String(os.Getenv("/tweets.tar.gz")),
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer s3file.Body.Close()
+
+	file, err := os.Create("/tmp/tweets.tar.gz")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, s3file.Body); err != nil {
+		panic(err)
+	}
+
+	if err := exec.Command("sh", "-c", "cd /tmp && tar Jxvf tweets.tar.xz").Run(); err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open("sqlite3", "/tmp/tweets.db")
 	if err != nil {
 		panic(err)
 	}
@@ -33,12 +64,32 @@ func main() {
 	}
 	tx := db.Begin()
 	for _, tweet := range tweets {
-		tweetDb := Tweet{
-			Tweet:     tweet.Text,
-			TwitterID: fmt.Sprint(tweet.Id),
+		var tweetDb Tweet
+		if db.Where("twitter_id = ?", fmt.Sprint(tweet.Id)).First(&tweetDb).RecordNotFound() {
+			tweetDb := Tweet{
+				Tweet:     tweet.Text,
+				TwitterID: fmt.Sprint(tweet.Id),
+			}
+			tx.Create(&tweetDb)
 		}
-		tx.Create(&tweetDb)
 	}
 	tx.Commit()
 
+	if err := exec.Command("sh", "-c", "cd /tmp && tar Jcvf tweets.tar.xz tweets.db").Run(); err != nil {
+		panic(err)
+	}
+
+	file2, err := os.Open("/tmp/tweets.tar.xz")
+
+	if _, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("AWS_S3_BUCKET")),
+		Key:    aws.String("/tweets.tar.xz"),
+		Body:   file2,
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := exec.Command("sh", "-c", "rm -rf /tmp/*").Run(); err != nil {
+		panic(err)
+	}
 }
